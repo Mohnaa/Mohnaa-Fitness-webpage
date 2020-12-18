14ce47966d92ff204cd19df3818d992949a3773f
Mohnaa Ghosh <71881679+Mohnaa@users.noreply.github.com>
2020-12-18 14:11:19 +0530

    first commit

---
+++ booyah.py
@@ -0,0 +1,11 @@
+
+print("Hello")
+x = float(input("Enter your income: "))
+y = 0.15
+z= 400000
+u = (x-z)*y
+if(x>400000):
+    print("your income tax is ", u)
+else:
+    print("you need to pay income tax")    
+
